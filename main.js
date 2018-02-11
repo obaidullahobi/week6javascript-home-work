87d41f46612e18ee2acf1038737349531af94106
obaidullahobi <33807026+obaidullahobi@users.noreply.github.com>
2018-02-11 11:35:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+	function myFunction() {
+	let x = document.getElementById("container1");
+	if (x.style.display === "none"){
+		x.style.display = "block";
+	}
+	else{
+		x.style.display = "none";
+	}
+}
